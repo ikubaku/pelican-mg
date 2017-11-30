54b297bc30a79db9d81f9498ef9d7617d74a2e5d
Lucas Cimon <lucas.cimon@gmail.com>
2017-11-30 03:49:39 +0100

    WIP

--- test_ludochaordic.sh
+++ test_ludochaordic.sh
@@ -12,6 +12,9 @@ cd ludochaordic
 
 pip install pelican markdown beautifulsoup4 pillow
 make DEBUG=1 OUTPUTDIR=output html
+# crash HERE because of missing imgs -> TODO:
+# - ./list_imgs_from_mds.py *.md -> currently incaple to extract img from content/2015-07-24-youtube_playlist_watcher
+# - create dummy jpg / png / gif files for each
 
 csslint --ignore=order-alphabetical output/theme/css/main.css
 
