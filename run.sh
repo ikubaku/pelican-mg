8e23860b4ff5fc8922df2fd3289750a4b9267610
Lucas Cimon <lucas.cimon@gmail.com>
2019-02-13 21:02:26 +0100

    Renommage bundle JS

--- run.sh
+++ run.sh
@@ -5,12 +5,12 @@
 set -o pipefail -o errexit -o nounset -o xtrace
 
 install () {
-    pip install pelican markdown beautifulsoup4 pillow
+    pip install beautifulsoup4 html5lib html5validator markdown pelican pillow
     ./gen_statics_bundles.py || true
 }
 
 install_dev () {
-    npm install -g eslint eslint-config-strict eslint-plugin-filenames stylelint
+    npm install -g eslint eslint-config-strict eslint-plugin-filenames htmlhint stylelint
     npm install stylelint-config-standard
     pip install pre-commit
     pre-commit install
@@ -18,16 +18,14 @@ install_dev () {
 
 test_ludochaordic () {
     cd ..
-    git clone https://github.com/getpelican/pelican-plugins.git
-    cd pelican-plugins
-    git submodule update --init image_process representative_image tag_cloud
-
-    cd ..
-    git clone https://github.com/Lucas-C/ludochaordic.git
+    if ! [ -d pelican-plugins ]; then
+        git clone https://github.com/getpelican/pelican-plugins.git
+        cd pelican-plugins
+        git submodule update --init image_process representative_image tag_cloud
+        cd ..
+    fi
+    [ -d ludochaordic ] || git clone https://github.com/Lucas-C/ludochaordic.git
     cd ludochaordic
-
-    npm install -g htmlhint
-    pip install html5lib html5validator
 
     ../pelican-mg/gen_imgs_from_mds.py content/*.md
     make DEBUG=1 OUTPUTDIR=output publish
@@ -35,7 +33,7 @@ test_ludochaordic () {
     # Too many missing img alt attributes in thoses:
     rm output/street-art-and-hedonogeolostism-in-london.html output/variante-2-joueurs-pour-bang-le-jeu-de-des.html
 
-    html5validator --root output/ --ignore-re='.*(Element "style" not allowed as child of element.*|Text not allowed in element "iframe" in this context.|No "p" element in scope but a "p" end tag seen.)' # issue with pelican renderer: <p> contains legally only inline-/inline-block-elements
+    html5validator --root output/ --ignore-re='.*(Element "style" not allowed as child of element.*|Text not allowed in element "iframe" in this context.|No "p" element in scope but a "p" end tag seen.|End tag "p" implied, but there were open elements.|Unclosed element "a".|End tag "div" seen, but there were open elements.)' # issue with pelican renderer: <p> contains legally only inline-/inline-block-elements
 
     cp ../pelican-mg/.htmlhintrc output/
     htmlhint output/
